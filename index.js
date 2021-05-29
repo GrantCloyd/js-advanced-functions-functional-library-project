4f192564bfe14ea8884e33a914fd4e41712ffeba
Grant Cloyd <dgcloyd@gmail.com>
2021-05-29 16:23:21 -0500

    commit tree 4e7ddaa4215a7860499d9479c4953dae887d4fd4

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-const fi = (function() {
-  return {
-    libraryMethod: function() {
-      return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
-    },
+const fi = (function () {
+   return {
+      libraryMethod: function () {
+         return "Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0"
+      },
 
-    each: function() {
+      each: function (coll, cback) {
+         let collValues = Object.values(coll)
+         coll.forEach(el => cback(el))
+         collValues.forEach(val => cback(val))
 
-    },
+         return coll
+      },
 
-    map: function() {
+      map: function () {},
 
-    },
+      reduce: function () {},
 
-    reduce: function() {
-
-    },
-
-    functions: function() {
-
-    },
-
-
-  }
+      functions: function () {},
+   }
 })()
+
+console.log(fi.each([1, 2, 3], alert))
 
 fi.libraryMethod()
